4a4530876f292dd83dd20846e5e233c801715a2d
Maksym Leshchuk <max.leshchuk@gmail.com>
2024-12-23 23:57:36 +0200

    -

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -13,11 +13,11 @@ namespace ASP_NET_core_Project.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            //modelBuilder.Entity<Category>().HasData(
-            //    new Category {Id = Guid.NewGuid(), Name = "Action", DisplayOrder = 1 },
-            //    new Category { Id = Guid.NewGuid(), Name = "SciFi", DisplayOrder = 2 },
-            //    new Category { Id = Guid.NewGuid(), Name = "History", DisplayOrder = 3 }
-            //);
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+            );
         }
     }
 }
--- Models/Category.cs
+++ Models/Category.cs
@@ -10,13 +10,13 @@ namespace ASP_NET_core_Project.Models
     public class Category
     {
         [Key] 
-        public Guid Id { get; set; }
+        public int Id { get; set; }
         [Required]
-        //[MaxLength(30)]
+        [MaxLength(30)]
         [DisplayName("Category Name")]
         public string Name { get; set; } = "";
         [DisplayName("Display Order")]
-        //[Range(1,100, ErrorMessage = "Display Order must be between 1-100")]
+        [Range(1,100, ErrorMessage = "Display Order must be between 1-100")]
         public int DisplayOrder { get; set; }
     }
 }
